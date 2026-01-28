0df9c39cf6f5c65554a35288dbf968aa520fd2a1
User <nikolajevs04@gmail.com>
2026-01-28 11:01:33 +0200

    fifth commit

--- Fruits.php
+++ Fruits.php
@@ -14,11 +14,11 @@ $ages =[
 echo $ages["Bobs"]."\n";
 
 //uzd3
-$students =array(
+$students =[
     array('Džons', 20, (90+85+88)/3),
     array('Džena', 22, (92+80+84)/3),
     array('Džo', 21, (78+85+90)/3)
-);
+];
 
  for($i=0;$i<3;$i++){
     for($j=0;$j<3;$j++){
@@ -27,11 +27,11 @@ $students =array(
     }
  }
  //uzd 4
- $students2 =array(
+ $students2 =[
     array('Džons', 20, 90,85,88),
     array('Džena', 22, 92,80,84),
     array('Džo', 21, 78,85,90)
-);
+ ];
 for($i=0;$i<3;$i++){
     for($j=0;$j<5;$j++){
         echo $students2[$i][$j]."\n";
@@ -52,3 +52,34 @@ $animals = [
 foreach ($animals as $animal) {
     var_dump($animal);
 }
+//uzd 6
+$colors=['red', 'blue', 'green', 'yellow'];
+// echo "Colors:";
+// foreach($colors as $color){
+//     echo $color.", ";
+// }
+// $result= "Colors: ";
+// foreach($colors as $color){
+//    $result =$result. $color.", ";
+// }
+
+// echo $result;
+echo "Colors: ";
+for ($i = 1; $i < count($colors); $i++) {
+    echo $colors[$i-1].", ";
+    if($i == count($colors)-1){
+        echo $colors[$i].".";
+    }
+}
+echo "\n";
+///////////////////////////////////////////
+echo "Colors: ";
+for ($i = 0; $i < count($colors); $i++) {
+    if ($i > 0) {
+        echo ", ";      
+    }
+    
+    echo $colors[$i];
+}
+echo ".";
+//Chat gpt ^
