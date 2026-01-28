63c1d0a4b97110c29ef7e057c8fd619ca5d171e4
User <nikolajevs04@gmail.com>
2026-01-28 08:51:20 +0200

    second commit

--- Fruits.php
+++ Fruits.php
@@ -14,18 +14,15 @@ $ages =[
 echo $ages["Bobs"]."\n";
 
 //uzd3
-$result = 0;
-$result2 = 0;
-$result3 = 0;
 $students =array(
-    array('Džons', 20, [90+85+88/3=>$result]),
-    array('Džena', 22, [92+80+84/3=>$result2]),
-    array('Džo', 21, [$result3<=(78+85+90)/3=>$result3])
+    array('Džons', 20, (90+85+88)/3),
+    array('Džena', 22, (92+80+84)/3),
+    array('Džo', 21, (78+85+90)/3)
 );
 
  for($i=0;$i<3;$i++){
     for($j=0;$j<3;$j++){
-        echo $students[$i][$j];
+        echo $students[$i][$j]."\n";
 
     }
  }
